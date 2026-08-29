285294eff03575783d96330d20ff60a42ab9978f
José Rebelo <joserebelo@outlook.com>
2026-08-29 13:18:33 +0100

    Fix crash on some Android 13 ROMs (#6657)

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/generic_scale/GenericWeightScaleMeasurementActivity.kt
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/generic_scale/GenericWeightScaleMeasurementActivity.kt
@@ -21,7 +21,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -54,12 +53,8 @@ class GenericWeightScaleMeasurementActivity : AbstractGBActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        device = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableExtra(GBDevice.EXTRA_DEVICE, GBDevice::class.java)
-        } else {
-            @Suppress("DEPRECATION")
-            intent.getParcelableExtra(GBDevice.EXTRA_DEVICE)
-        } ?: throw IllegalArgumentException(GBDevice.EXTRA_DEVICE + " must not be null")
+        device = intent.getParcelableCompat<GBDevice>(GBDevice.EXTRA_DEVICE)
+            ?: throw IllegalArgumentException(GBDevice.EXTRA_DEVICE + " must not be null")
 
         unit = GBApplication.getPrefs().weightUnit
 
