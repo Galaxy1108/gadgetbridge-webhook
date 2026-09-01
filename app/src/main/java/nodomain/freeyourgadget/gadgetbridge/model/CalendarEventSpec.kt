Paslestrange/IncidentBridge

commit 2fbef8575346030ad95a44626a7a1f52d552c4b1

Calendar Sync: Add the option to filter events
This change adds filter filters to the calendar sync options:
  1. Declined events
  2. Canceled events
  3. Color Label filtering
  4. Focus Time event filtering
  5. Working Location event filtering
  6. All Day Event filtering

This changes also inverts the menu for teh sync black list so that is
now a white list. The underlying prefs variables have not changed, just
how the interface displays them so existing user settings will carry
over.

The color label filtering is more useful on calendars from Google
Workspace Calendars that actually have named colors. There is no way
that I can find to access the names of the colors to show to the user,
but it can still be useful to tag some events that you don't want synced.

In addition to the color filters, Google Workspace Calendar has extended
parameters called Focus Time and Working Location. Working location
appears as an all day event every day and if you use the Focus Time as a
Calendar block, your calendar may be filled with these events. For my
work calendar these two event types choke out the "real" events, so it
is nice to filter them.

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/impl/GBDeviceService.java
@@ -20,6 +20,8 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge.impl;
 
+import static nodomain.freeyourgadget.gadgetbridge.util.JavaExtensions.coalesce;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -28,6 +30,11 @@ import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+
+import androidx.annotation.NonNull;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.UUID;
@@ -50,13 +57,6 @@ import nodomain.freeyourgadget.gadgetbridge.model.Reminder;
 import nodomain.freeyourgadget.gadgetbridge.model.WorldClock;
 import nodomain.freeyourgadget.gadgetbridge.service.DeviceCommunicationService;
 import nodomain.freeyourgadget.gadgetbridge.util.RtlUtils;
-
-import static nodomain.freeyourgadget.gadgetbridge.util.JavaExtensions.coalesce;
-
-import androidx.annotation.NonNull;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class GBDeviceService implements DeviceService {
@@ -456,6 +456,7 @@ public class GBDeviceService implements DeviceService {
     public void onAddCalendarEvent(CalendarEventSpec calendarEventSpec) {
         Intent intent = createIntent().setAction(ACTION_ADD_CALENDAREVENT)
                 .putExtra(EXTRA_CALENDAREVENT_ID, calendarEventSpec.id)
+                .putExtra(EXTRA_CALENDAREVENT_EVENT_ID, calendarEventSpec.eventId)
                 .putExtra(EXTRA_CALENDAREVENT_TYPE, calendarEventSpec.type)
                 .putExtra(EXTRA_CALENDAREVENT_TIMESTAMP, calendarEventSpec.timestamp)
                 .putExtra(EXTRA_CALENDAREVENT_DURATION, calendarEventSpec.durationInSeconds)
@@ -464,8 +465,11 @@ public class GBDeviceService implements DeviceService {
                 .putExtra(EXTRA_CALENDAREVENT_TITLE, calendarEventSpec.title)
                 .putExtra(EXTRA_CALENDAREVENT_DESCRIPTION, calendarEventSpec.description)
                 .putExtra(EXTRA_CALENDAREVENT_CALNAME, calendarEventSpec.calName)
+                .putExtra(EXTRA_CALENDAREVENT_CALENDAR_COLOR, calendarEventSpec.calendarColor)
                 .putExtra(EXTRA_CALENDAREVENT_COLOR, calendarEventSpec.color)
-                .putExtra(EXTRA_CALENDAREVENT_LOCATION, calendarEventSpec.location);
+                .putExtra(EXTRA_CALENDAREVENT_LOCATION, calendarEventSpec.location)
+                .putExtra(EXTRA_CALENDAREVENT_STATUS, calendarEventSpec.location)
+                .putExtra(EXTRA_CALENDAREVENT_ATTENDING_STATUS, calendarEventSpec.location);
         invokeService(intent);
     }
 
@@ -473,6 +477,7 @@ public class GBDeviceService implements DeviceService {
     public void onDeleteCalendarEvent(byte type, long id) {
         Intent intent = createIntent().setAction(ACTION_DELETE_CALENDAREVENT)
                 .putExtra(EXTRA_CALENDAREVENT_TYPE, type)
+                // TODO: If swapping to EVENT_ID, change this here.
                 .putExtra(EXTRA_CALENDAREVENT_ID, id);
         invokeService(intent);
     }

app/src/main/AndroidManifest.xml
@@ -287,8 +287,8 @@
             android:label="@string/title_activity_notification_management"
             android:parentActivityName=".activities.SettingsActivity" />
         <activity
-            android:name=".activities.CalBlacklistActivity"
-            android:label="@string/title_activity_calblacklist"
+            android:name=".activities.CalendarSelectionActivity"
+            android:label="@string/pref_title_calendar_sync_choose"
             android:parentActivityName=".activities.SettingsActivity" />
         <activity
             android:name=".devices.vesc.VescControlActivity"

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/CalBlacklistActivity.java
@@ -1,171 +1,0 @@
-/*  Copyright (C) 2017-2024 Carsten Pfeiffer, Daniele Gobbetti, José Rebelo,
-    Ludovic Jozeau
-
-    This file is part of Gadgetbridge.
-
-    Gadgetbridge is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Gadgetbridge is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
-package nodomain.freeyourgadget.gadgetbridge.activities;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Paint;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.CalendarContract;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NavUtils;
-import nodomain.freeyourgadget.gadgetbridge.GBApplication;
-import nodomain.freeyourgadget.gadgetbridge.R;
-import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
-import nodomain.freeyourgadget.gadgetbridge.util.GB;
-import nodomain.freeyourgadget.gadgetbridge.util.calendar.CalendarManager;
-
-
-public class CalBlacklistActivity extends AbstractGBActivity {
-
-    private final String[] EVENT_PROJECTION = new String[]{
-            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
-            CalendarContract.Calendars.ACCOUNT_NAME,
-            CalendarContract.Calendars.CALENDAR_COLOR
-    };
-    private ArrayList<Calendar> calendarsArrayList;
-
-    private GBDevice gbDevice;
-    private CalendarManager calendarManager;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calblacklist);
-        ListView calListView = (ListView) findViewById(R.id.calListView);
-
-        gbDevice = getIntent().getParcelableExtra(GBDevice.EXTRA_DEVICE);
-        calendarManager = new CalendarManager(this, gbDevice.getAddress());
-
-        final Uri uri = CalendarContract.Calendars.CONTENT_URI;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            GB.toast(this, "Calendar permission not granted. Nothing to do.", Toast.LENGTH_SHORT, GB.WARN);
-            return;
-        }
-        try (Cursor cur = getContentResolver().query(uri, EVENT_PROJECTION, null, null, null)) {
-            calendarsArrayList = new ArrayList<>();
-            while (cur != null && cur.moveToNext()) {
-                calendarsArrayList.add(new Calendar(cur.getString(0), cur.getString(1), cur.getInt(2)));
-            }
-        }
-
-        ArrayAdapter<Calendar> calAdapter = new CalendarListAdapter(this, calendarsArrayList);
-        calListView.setAdapter(calAdapter);
-        calListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
-                Calendar item = calendarsArrayList.get(i);
-                CheckBox selected = (CheckBox) view.findViewById(R.id.item_checkbox);
-                toggleEntry(view);
-                if (selected.isChecked()) {
-                    calendarManager.addCalendarToBlacklist(item.getUniqueString());
-                } else {
-                    calendarManager.removeFromCalendarBlacklist(item.getUniqueString());
-                }
-            }
-        });
-
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item) {
-        final int itemId = item.getItemId();
-        if (itemId == android.R.id.home) {
-            NavUtils.navigateUpFromSameTask(this);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void toggleEntry(View view) {
-        TextView name = (TextView) view.findViewById(R.id.calendar_name);
-        CheckBox checked = (CheckBox) view.findViewById(R.id.item_checkbox);
-
-        name.setPaintFlags(name.getPaintFlags() ^ Paint.STRIKE_THRU_TEXT_FLAG);
-        checked.toggle();
-    }
-
-    class Calendar {
-        private final String displayName;
-        private final String accountName;
-        private final int color;
-
-        public Calendar(String displayName, String accountName, int color) {
-            this.displayName = displayName;
-            this.accountName = accountName;
-            this.color = color;
-        }
-
-        public String getUniqueString() {
-            return accountName + '/' + displayName;
-        }
-    }
-
-    private class CalendarListAdapter extends ArrayAdapter<Calendar> {
-
-        CalendarListAdapter(@NonNull Context context, @NonNull List<Calendar> calendars) {
-            super(context, 0, calendars);
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
-            Calendar item = getItem(position);
-
-            if (view == null) {
-                LayoutInflater inflater = (LayoutInflater) super.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(R.layout.item_cal_blacklist, parent, false);
-            }
-
-            View color = view.findViewById(R.id.calendar_color);
-            TextView name = (TextView) view.findViewById(R.id.calendar_name);
-            TextView ownerAccount = (TextView) view.findViewById(R.id.calendar_owner_account);
-            CheckBox checked = (CheckBox) view.findViewById(R.id.item_checkbox);
-
-            if (calendarManager.calendarIsBlacklisted(item.getUniqueString()) && !checked.isChecked() ||
-                    !calendarManager.calendarIsBlacklisted(item.getUniqueString()) && checked.isChecked()) {
-                toggleEntry(view);
-            }
-            color.setBackgroundColor(item.color);
-            name.setText(item.displayName);
-            ownerAccount.setText(item.accountName);
-
-            return view;
-        }
-    }
-}

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/CalendarSelectionActivity.java
@@ -1,0 +1,236 @@
+/*  Copyright (C) 2017-2024 Carsten Pfeiffer, Daniele Gobbetti, José Rebelo,
+    Ludovic Jozeau
+
+    This file is part of Gadgetbridge.
+
+    Gadgetbridge is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gadgetbridge is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+package nodomain.freeyourgadget.gadgetbridge.activities;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NavUtils;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.List;
+
+import nodomain.freeyourgadget.gadgetbridge.GBApplication;
+import nodomain.freeyourgadget.gadgetbridge.R;
+import nodomain.freeyourgadget.gadgetbridge.activities.devicesettings.DeviceSettingsPreferenceConst;
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice;
+import nodomain.freeyourgadget.gadgetbridge.util.GB;
+import nodomain.freeyourgadget.gadgetbridge.util.GBPrefs;
+import nodomain.freeyourgadget.gadgetbridge.util.calendar.CalendarManager;
+
+
+public class CalendarSelectionActivity extends AbstractGBActivity {
+    private GBDevice gbDevice;
+    private CalendarManager calendarManager;
+    private List<CalendarManager.CalendarEntry> calendars;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_selection);
+        ListView calListView = (ListView) findViewById(R.id.calendar_selection_list_view);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) !=
+            PackageManager.PERMISSION_GRANTED) {
+            GB.toast(this, "Calendar permission not granted. Nothing to do.", Toast.LENGTH_SHORT,
+                     GB.WARN);
+            return;
+        }
+
+        gbDevice = getIntent().getParcelableExtra(GBDevice.EXTRA_DEVICE);
+        calendarManager = new CalendarManager(this, gbDevice.getAddress());
+        calendars = calendarManager.getCalendars();
+
+        CalendarListAdapter calAdapter = new CalendarListAdapter(this, calendarManager, calendars);
+        calListView.setAdapter(calAdapter);
+        calListView.setOnItemClickListener(calAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        final int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class CalendarListAdapter extends ArrayAdapter<CalendarManager.CalendarEntry>
+            implements AdapterView.OnItemClickListener {
+        private final CalendarManager calendarManager;
+
+        CalendarListAdapter(@NonNull Context context, CalendarManager calendarManager,
+                            @NonNull List<CalendarManager.CalendarEntry> calendars) {
+            super(context, 0, calendars);
+            this.calendarManager = calendarManager;
+        }
+
+        private void setRowStyle(View view, boolean enabled) {
+            // Set the state of the checkbox.
+            CheckBox checked = (CheckBox) view.findViewById(R.id.item_checkbox);
+            checked.setChecked(enabled);
+
+            // Make the color slightly transparent when disabled.
+            View colorBox = view.findViewById(R.id.calendar_color);
+            Drawable background = colorBox.getBackground();
+            background.setAlpha(enabled ? 0xFF : 0x2F);
+            colorBox.setBackground(background);
+
+            // Grey out the text if not enabled.
+            view.findViewById(R.id.calendar_name).setEnabled(enabled);
+            view.findViewById(R.id.calendar_owner_account).setEnabled(enabled);
+
+            // If the color selection button is enabled, make it visible if the row is also enabled.
+            ImageView colorButton = (ImageView) view.findViewById(R.id.calendar_color_drop_down);
+            colorButton.setVisibility(
+                    (enabled && colorButton.isEnabled()) ? View.VISIBLE : View.GONE);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
+            CalendarManager.CalendarEntry item = getItem(position);
+
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) super.getContext().getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.item_calendar_selection, parent, false);
+            }
+
+            // Populate each element
+            ((TextView) view.findViewById(R.id.calendar_name)).setText(item.displayName());
+            ((TextView) view.findViewById(R.id.calendar_owner_account)).setText(item.accountName());
+            view.findViewById(R.id.calendar_color).setBackgroundColor(item.color());
+            View colorDropDown = view.findViewById(R.id.calendar_color_drop_down);
+            colorDropDown.setEnabled(!item.eventColors().isEmpty());
+
+            if (!item.eventColors().isEmpty()) {
+                colorDropDown.setOnClickListener((button) -> {
+                    // Create a new layout for the dialog
+                    LayoutInflater inflater =
+                            (LayoutInflater) super.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    View colorView =
+                            inflater.inflate(R.layout.activity_calendar_color_selection, null);
+
+                    GridView colorListView =
+                            (GridView) colorView.findViewById(R.id.calendar_color_selection_list_view);
+
+                    CalendarColorGridAdapter colorAdaptor =
+                            new CalendarColorGridAdapter(super.getContext(), calendarManager,
+                                                         item.getUniqueString(), item.eventColors().stream().sorted().toList());
+                    colorListView.setAdapter(colorAdaptor);
+                    colorListView.setOnItemClickListener(colorAdaptor);
+
+                    new MaterialAlertDialogBuilder(getContext())
+                        .setTitle(R.string.pref_title_calendar_sync_colors)
+                        .setView(colorView).setIcon(R.drawable.ic_calendar_cancel)
+                        .setPositiveButton(R.string.done, null)
+                        .show();
+                });
+            }
+
+            setRowStyle(view, !calendarManager.isCalendarBlacklisted(item.getUniqueString()));
+            return view;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            CalendarManager.CalendarEntry item = getItem(position);
+            // Invert the checkbox on click before checking it's state below.
+            CheckBox selected = (CheckBox) view.findViewById(R.id.item_checkbox);
+            selected.toggle();
+
+            setRowStyle(view, selected.isChecked());
+
+            if (selected.isChecked()) {
+                calendarManager.removeFromCalendarBlacklist(item.getUniqueString());
+            } else {
+                calendarManager.addCalendarToBlacklist(item.getUniqueString());
+            }
+
+            GBApplication.deviceService(gbDevice).onSendConfiguration(GBPrefs.CALENDAR_BLACKLIST);
+        }
+    }
+
+    private class CalendarColorGridAdapter extends ArrayAdapter<Integer>
+            implements AdapterView.OnItemClickListener {
+        private final CalendarManager calendarManager;
+        private final String calendarUniqueName;
+
+        CalendarColorGridAdapter(@NonNull Context context, @NonNull CalendarManager calendarManager,
+                                 @NonNull String calendarUniqueName,
+                                 @NonNull List<Integer> colors) {
+            super(context, 0, colors);
+            this.calendarManager = calendarManager;
+            this.calendarUniqueName = calendarUniqueName;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View view, @NonNull ViewGroup parent) {
+            int color = getItem(position);
+
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) super.getContext().getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.item_calendar_color_selection, parent, false);
+            }
+            CheckBox checked = (CheckBox) view.findViewById(R.id.item_event_color);
+            checked.setChecked(!calendarManager.isColorBlacklistedForCalendar(calendarUniqueName, color));
+            checked.setButtonTintList(ColorStateList.valueOf(0xFF000000 | color));
+            checked.setBackgroundColor(0xFF000000 | color);
+            return view;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            int color = getItem(position);
+            // Invert the checkbox on click before checking it's state below.
+            CheckBox selected = (CheckBox) view.findViewById(R.id.item_event_color);
+            selected.toggle();
+            if (selected.isChecked()) {
+                calendarManager.removeColorFromBlacklistForCalendar(calendarUniqueName, color);
+            } else {
+                calendarManager.addColorToBlacklistForCalendar(calendarUniqueName, color);
+            }
+            GBApplication.deviceService(gbDevice).onSendConfiguration(
+                    DeviceSettingsPreferenceConst.PREF_CALENDAR_SYNC_COLOR_BLACKLIST);
+        }
+    }
+}

Galaxy1108/gadgetbridge-webhook

commit 07816ffd696d8dcbb984d800d7ce0ffe0492dcd2

CalendarEvent: move text modifications to Spec methods
Also fix a regression introduced by 2fbef8575346030ad95a44626a7a1f52d552c4b1 on the serialization to Intent

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/impl/GBDeviceService.java
@@ -72,10 +72,6 @@ public class GBDeviceService implements DeviceService {
     private final GBDevice mDevice;
     private final Class<? extends Service> mServiceClass;
     public static final String[] transliterationExtras = new String[]{
-            EXTRA_CALENDAREVENT_TITLE,
-            EXTRA_CALENDAREVENT_DESCRIPTION,
-            EXTRA_CALENDAREVENT_LOCATION,
-            EXTRA_CALENDAREVENT_CALNAME,
     };
     private final Executor mainExecutor = ContextCompat.getMainExecutor(GBApplication.getContext());
 
@@ -495,22 +491,24 @@ public class GBDeviceService implements DeviceService {
 
     @Override
     public void onAddCalendarEvent(@NonNull CalendarEventSpec calendarEventSpec) {
+        final CalendarEventSpec withRtlFix = calendarEventSpec.withRtlFix();
+
         Intent intent = createIntent().setAction(ACTION_ADD_CALENDAREVENT)
-                .putExtra(EXTRA_CALENDAREVENT_ID, calendarEventSpec.getId())
-                .putExtra(EXTRA_CALENDAREVENT_EVENT_ID, calendarEventSpec.getEventId())
-                .putExtra(EXTRA_CALENDAREVENT_TYPE, calendarEventSpec.getType())
-                .putExtra(EXTRA_CALENDAREVENT_TIMESTAMP, calendarEventSpec.getTimestamp())
-                .putExtra(EXTRA_CALENDAREVENT_DURATION, calendarEventSpec.getDurationInSeconds())
-                .putExtra(EXTRA_CALENDAREVENT_ALLDAY, calendarEventSpec.getAllDay())
-                .putExtra(EXTRA_CALENDAREVENT_REMINDERS, calendarEventSpec.getReminders())
-                .putExtra(EXTRA_CALENDAREVENT_TITLE, calendarEventSpec.getTitle())
-                .putExtra(EXTRA_CALENDAREVENT_DESCRIPTION, calendarEventSpec.getDescription())
-                .putExtra(EXTRA_CALENDAREVENT_CALNAME, calendarEventSpec.getCalName())
-                .putExtra(EXTRA_CALENDAREVENT_CALENDAR_COLOR, calendarEventSpec.getCalendarColor())
-                .putExtra(EXTRA_CALENDAREVENT_COLOR, calendarEventSpec.getColor())
-                .putExtra(EXTRA_CALENDAREVENT_LOCATION, calendarEventSpec.getLocation())
-                .putExtra(EXTRA_CALENDAREVENT_STATUS, calendarEventSpec.getLocation())
-                .putExtra(EXTRA_CALENDAREVENT_ATTENDING_STATUS, calendarEventSpec.getLocation());
+                .putExtra(EXTRA_CALENDAREVENT_ID, withRtlFix.getId())
+                .putExtra(EXTRA_CALENDAREVENT_EVENT_ID, withRtlFix.getEventId())
+                .putExtra(EXTRA_CALENDAREVENT_TYPE, withRtlFix.getType())
+                .putExtra(EXTRA_CALENDAREVENT_TIMESTAMP, withRtlFix.getTimestamp())
+                .putExtra(EXTRA_CALENDAREVENT_DURATION, withRtlFix.getDurationInSeconds())
+                .putExtra(EXTRA_CALENDAREVENT_ALLDAY, withRtlFix.getAllDay())
+                .putExtra(EXTRA_CALENDAREVENT_REMINDERS, withRtlFix.getReminders())
+                .putExtra(EXTRA_CALENDAREVENT_TITLE, withRtlFix.getTitle())
+                .putExtra(EXTRA_CALENDAREVENT_DESCRIPTION, withRtlFix.getDescription())
+                .putExtra(EXTRA_CALENDAREVENT_CALNAME, withRtlFix.getCalName())
+                .putExtra(EXTRA_CALENDAREVENT_CALENDAR_COLOR, withRtlFix.getCalendarColor())
+                .putExtra(EXTRA_CALENDAREVENT_COLOR, withRtlFix.getColor())
+                .putExtra(EXTRA_CALENDAREVENT_LOCATION, withRtlFix.getLocation())
+                .putExtra(EXTRA_CALENDAREVENT_STATUS, withRtlFix.getStatus())
+                .putExtra(EXTRA_CALENDAREVENT_ATTENDING_STATUS, withRtlFix.getAttendingStatus());
         invokeService(intent);
     }
 

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/DeviceActionHandler.java
@@ -189,7 +189,7 @@ public class DeviceActionHandler {
                 calendarEventSpec.setColor(intentCopy.getIntExtra(EXTRA_CALENDAREVENT_COLOR, 0));
                 calendarEventSpec.setStatus(intentCopy.getIntExtra(EXTRA_CALENDAREVENT_STATUS, 0));
                 calendarEventSpec.setAttendingStatus(intentCopy.getIntExtra(EXTRA_CALENDAREVENT_ATTENDING_STATUS, 0));
-                deviceSupport.onAddCalendarEvent(calendarEventSpec);
+                deviceSupport.onAddCalendarEvent(calendarEventSpec.transliterated(deviceSupport, device.getDeviceCoordinator(), device, transliterator));
                 break;
             }
             case ACTION_DELETE_CALENDAREVENT: {

app/src/main/java/nodomain/freeyourgadget/gadgetbridge/model/CalendarEventSpec.kt
@@ -17,6 +17,13 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
 package nodomain.freeyourgadget.gadgetbridge.model
 
+import nodomain.freeyourgadget.gadgetbridge.devices.DeviceCoordinator
+import nodomain.freeyourgadget.gadgetbridge.impl.GBDevice
+import nodomain.freeyourgadget.gadgetbridge.service.DeviceSupport
+import nodomain.freeyourgadget.gadgetbridge.util.RtlUtils
+import nodomain.freeyourgadget.gadgetbridge.util.language.Transliterator
+import nodomain.freeyourgadget.gadgetbridge.util.sanitizeText
+
 data class CalendarEventSpec @JvmOverloads constructor(
     var type: Byte = TYPE_UNKNOWN,
     var id: Long = 0,
@@ -33,7 +40,34 @@ data class CalendarEventSpec @JvmOverloads constructor(
     var reminders: ArrayList<Long>? = null,
     var status: Int = 0,
     var attendingStatus: Int = 0
-) {
+) : DeviceTextAdaptable<CalendarEventSpec> {
+    override fun transliterated(
+        deviceSupport: DeviceSupport,
+        deviceCoordinator: DeviceCoordinator,
+        device: GBDevice,
+        transliterator: Transliterator?
+    ): CalendarEventSpec {
+        fun transform(text: String?): String? {
+            val sanitized = sanitizeText(deviceSupport, deviceCoordinator, device, text)
+            return transliterator?.let { sanitized?.let(it::transliterate) } ?: sanitized
+        }
+        return copy(
+            title = transform(title),
+            description = transform(description),
+            location = transform(location),
+            calName = transform(calName)
+        )
+    }
+
+    override fun withRtlFix(): CalendarEventSpec {
+        if (!RtlUtils.rtlSupport()) return this
+        return copy(
+            title = title?.let(RtlUtils::fixRtl),
+            description = description?.let(RtlUtils::fixRtl),
+            location = location?.let(RtlUtils::fixRtl),
+            calName = calName?.let(RtlUtils::fixRtl)
+        )
+    }
 
     companion object {
         const val TYPE_UNKNOWN: Byte = 0