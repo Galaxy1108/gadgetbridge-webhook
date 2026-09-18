a57eff143058388b4eb261d98b5c47bb4113cd88
José Rebelo <joserebelo@outlook.com>
2026-09-18 18:30:36 +0100

    Bump schema version to 142

--- GBDaoGenerator/src/nodomain/freeyourgadget/gadgetbridge/daogen/GBDaoGenerator.java
+++ GBDaoGenerator/src/nodomain/freeyourgadget/gadgetbridge/daogen/GBDaoGenerator.java
@@ -110,7 +110,7 @@ public class GBDaoGenerator {
             outputDir.mkdirs();
         }
 
-        final Schema schema = new Schema(141, MAIN_PACKAGE + ".entities");
+        final Schema schema = new Schema(142, MAIN_PACKAGE + ".entities");
 
         final List<Entity> sampleProvidersToGenerate = new LinkedList<>();
         final List<Entity> batterySampleProvidersToGenerate = new LinkedList<>();
